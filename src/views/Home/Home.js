9a547dc8ff19ca0f634ab2e5ce2d62b1f2f2bb59
Berk Ozer <galipberkozer@gmail.com>
2020-04-10 16:07:05 -0400

    Add padding to Home with useStyles

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 // import { makeStyles } from '@material-ui/styles';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
 
 const Home = () => {
+  const classes = useStyles();
+
   return (
-    <Typography variant="h1">Hello</Typography>
+    <div className={classes.root}>
+      <Typography variant="h1">Hello</Typography>
+    </div>
   )
 }
 
