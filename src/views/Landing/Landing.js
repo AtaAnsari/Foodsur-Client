a2c529567de79821c29407f7fb09d15bc07d7436
Kevin <kevin.m.guertin@gmail.com>
2020-04-10 17:09:15 -0400

    add buttons and align header text

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/styles';
-// import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { Button, Container, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
 
 const Landing = () => {
+  const classes = useStyles()
   return (
-    <h1>Hello!!</h1>
+    <Container className={classes.root}>
+      <Typography variant='h1' className={classes.header} gutterBottom={true} align={'center'}>
+        Find the Right Food For You
+      </Typography>
+      <Button variant='contained' color='primary'>Login</Button>
+      <Button variant='contained' color='primary'>Sign up</Button>
+    </Container>
+
   )
 }
 
