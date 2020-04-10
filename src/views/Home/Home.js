ac4f55d485d490702f0efa5210f37fa20fc89592
Berk Ozer <galipberkozer@gmail.com>
2020-04-10 15:45:09 -0400

    Build Home component to test that it loads

---
+++ src/views/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/styles';
+// import { Grid } from '@material-ui/core';
+
+const Home = () => {
+  return (
+    <h1>Hello!!</h1>
+  )
+}
+
+export default Home;
