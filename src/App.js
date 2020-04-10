0ed7ee5dad30b382b4552977e5c73f312fded4c2
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-10 14:03:59 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Chart } from 'react-chartjs-2';
+import { ThemeProvider } from '@material-ui/styles';
+import validate from 'validate.js';
+
+import { chartjs } from './helpers';
+import theme from './theme';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+import validators from './common/validators';
+import Routes from './Routes';
+
+const browserHistory = createBrowserHistory();
+
+Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+  draw: chartjs.draw
+});
+
+validate.validators = {
+  ...validate.validators,
+  ...validators
+};
+
+export default class App extends Component {
+  // state = {users: []}
+
+  // componentDidMount() {
+  //   fetch('http://localhost:8080/api/users')
+  //     .then(res => {
+  //       console.log(res)
+  //       return res.json()
+  //     })
+
+  //     .then(users => this.setState({users}))
+  // }
+  render() {
+    return (
+      // <div className = "App">
+      //   <h1>Users</h1>
+      //   <ul>
+      //     {this.state.users.map(user =>
+      //       <li key={user.id}>{user.username}</li>
+      //     )}
+      //   </ul>
+      // </div>
+      
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    );
+  }
+}
