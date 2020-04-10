edecf598d04b0a9c70818377adc9d2009eb81d5d
Berk Ozer <galipberkozer@gmail.com>
2020-04-10 16:04:57 -0400

    Use Typography component to render text

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // import { makeStyles } from '@material-ui/styles';
-// import { Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <h1>Hello!!</h1>
+    <Typography variant="h1">Hello</Typography>
   )
 }
 
