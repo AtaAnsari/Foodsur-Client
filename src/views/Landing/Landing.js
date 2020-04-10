72d10c65ee2f074b8e92cbcdebb3e5b22ad1c5ef
Kevin <kevin.m.guertin@gmail.com>
2020-04-10 17:49:11 -0400

    add background image

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -4,7 +4,17 @@ import { Button, Container, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(4)
+    padding: theme.spacing(4),
+    backgroundImage: "url('/images/landing-bg.jpg')",
+    height: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed"
+  },
+  buttonStyle: {
+    marginTop: '0.5rem',
+    marginBottom: '1rem'
   }
 }));
 
@@ -15,8 +25,8 @@ const Landing = () => {
       <Typography variant='h1' className={classes.header} gutterBottom={true} align={'center'}>
         Find the Right Food For You
       </Typography>
-      <Button variant='contained' color='primary'>Login</Button>
-      <Button variant='contained' color='primary'>Sign up</Button>
+      <Button variant='contained' color='primary' fullWidth={true} className={classes.buttonStyle}>Login</Button>
+      <Button variant='contained' color='primary' fullWidth={true} className={classes.buttonStyle}>Sign up</Button>
     </Container>
 
   )
