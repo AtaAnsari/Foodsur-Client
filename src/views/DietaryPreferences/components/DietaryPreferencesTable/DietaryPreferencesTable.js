71616bc02c92f5b2b7ab3a200239e7e96dc987f7
Berk Ozer <galipberkozer@gmail.com>
2020-04-11 18:44:55 -0400

    Make generic table with one element in DietaryPreferencesTable

--- src/views/DietaryPreferences/components/DietaryPreferencesTable/DietaryPreferencesTable.js
+++ src/views/DietaryPreferences/components/DietaryPreferencesTable/DietaryPreferencesTable.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import {
+  Table,
+  TableRow,
+  TableCell,
+  Checkbox,
+  TableBody,
+  Paper
+} from '@material-ui/core';
 
 const DietaryPreferencesTable = () => {
   return (
-    <div>Hello</div>
+    <Paper>
+      <Table>
+        <TableBody>
+          <TableRow
+            // className={}
+            // hover
+            // key={}
+            // selected={}
+          >
+            <TableCell padding="checkbox">
+              <Checkbox
+                // checked={}
+                color="primary"
+                // onChange={}
+                // value="true"
+              />
+              Low-Fat
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </ Table>
+    </Paper>
   )
 }
 
