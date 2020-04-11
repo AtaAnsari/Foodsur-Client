cba609823690a61f452afaddb59bb3fb3a2802c8
Berk Ozer <galipberkozer@gmail.com>
2020-04-11 17:19:21 -0400

    Add info text in DietaryPreferences

--- src/views/DietaryPreferences/DietaryPreferences.js
+++ src/views/DietaryPreferences/DietaryPreferences.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
 
 const dietaryPreferences = () => {
+  const classes = useStyles();
+
   return (
-    <div>Hello</div>
+    <div className={classes.root}>
+      <Typography
+        align="center"
+        variant="h3"
+      >
+        Please choose any dietary preferences that apply to you:
+      </Typography>
+    </div>
   )
 }
 
