d8a7be69bafda813f8a3be6026ce7bb11bc703d4
Berk Ozer <galipberkozer@gmail.com>
2020-04-11 18:55:04 -0400

    Create useDietaryPreferences hook with dummy data

---
+++ src/hooks/useDietaryPreferences.js
@@ -0,0 +1,10 @@
+export default function useDietaryPreferences() {
+  const preferences = [
+    { id: 1, name: 'Low-fat'},
+    { id: 2, name: 'Low-carb'},
+    { id: 3, name: 'Vegan'},
+    { id: 4, name: 'Dairy-free'},
+  ]
+
+  return preferences;
+}
