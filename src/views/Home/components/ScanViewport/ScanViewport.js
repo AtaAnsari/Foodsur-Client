234ec1aadaf691e13d1be4750a9912a49735e9bf
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-10 21:27:17 -0400

    Add barcode button

--- src/views/Home/components/ScanViewport/ScanViewport.js
+++ src/views/Home/components/ScanViewport/ScanViewport.js
@@ -1,10 +1,35 @@
 import Scanner from '../Scanner/Scanner';
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
+import { Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import "../Scanner/styles.css";
+import BlockIcon from '@material-ui/icons/Block';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  },
+  scanButton: {
+    padding:'0',
+    width: '300px',
+    height: '200px'
+  },
+  barcode: {
+    width: '250px'
+  },
+  stop: {
+    width: '100px'
+  }
+}));
+
+
+
 
 const ScanViewport = () => {
+  const classes = useStyles();
+
   const [camera, setCamera] = useState(false);
   const [result, setResult] = useState(null);
 
@@ -15,9 +40,25 @@ const ScanViewport = () => {
   return (
     <div className="App">
       <p>{result ? result : "Scanning..."}</p>
-      <button onClick={() => setCamera(!camera)}>
-        {camera ? "Stop" : "Start"}
-      </button>
+
+      <Button
+          onClick={() => setCamera(!camera)}
+          color="primary"
+          variant="contained"
+          size="large"
+          className={classes.scanButton}
+        >
+          {camera ? <img
+            className= {classes.stop}
+            alt="STOP"
+            src="/images/logos/stop.png"
+          /> : <img
+            className= {classes.barcode}
+            alt="Logo"
+            src="/images/logos/barcode.png"
+          />}
+          
+        </Button>
       <div className="container">
         {camera && <Scanner onDetected={onDetected} />}
       </div>
@@ -26,3 +67,4 @@ const ScanViewport = () => {
 };
 
 export default ScanViewport;
+
