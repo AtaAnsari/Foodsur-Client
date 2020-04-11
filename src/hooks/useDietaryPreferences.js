61c3afcae7d645c0554b0375e142311196523925
Berk Ozer <galipberkozer@gmail.com>
2020-04-11 19:33:32 -0400

    Add more dummy data into useDietaryPreferences

--- src/hooks/useDietaryPreferences.js
+++ src/hooks/useDietaryPreferences.js
@@ -3,7 +3,10 @@ export default function useDietaryPreferences() {
     { id: 1, name: 'Low-fat'},
     { id: 2, name: 'Low-carb'},
     { id: 3, name: 'Vegan'},
-    { id: 4, name: 'Dairy-free'},
+    { id: 4, name: 'Vegetarian'},
+    { id: 5, name: 'Peanut-free'},
+    { id: 6, name: 'Sugar conscious'},
+    { id: 7, name: 'High-protein'}
   ]
 
   return preferences;
