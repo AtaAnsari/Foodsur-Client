126c0b982f1f0747bc73768b1a22c59e91864ebc
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-12 16:00:41 -0400

    isolate product data from Api response and store to state

--- src/views/Home/components/ScanViewport/useApiData.js
+++ src/views/Home/components/ScanViewport/useApiData.js
@@ -1,11 +1,12 @@
+
 import { useState, useEffect } from "react"
 import axios from "axios"
 
 export function useApiData() {
-  const [state, setState] = useState({
+  const [product, setProduct] = useState({
     healthTags: [],
     dietTags: [],
-    productName: ""
+    productName: "Monday"
   });
 
 
@@ -15,6 +16,12 @@ export function useApiData() {
 
     axios.get(upcUrl)
       .then(res => {
+        const name = res.data.hints[0].food.label
+        // setProduct((product) => ({
+        //   ...product, 
+        //   productName: name
+        //   }))
+        setProduct(product.productName = name)
         upcIngredients = {
           "ingredients": [
             {
@@ -24,103 +31,22 @@ export function useApiData() {
             }
           ]
         }
-        console.log(upcIngredients);
       })
+
       .then(res => axios.post(`https://api.edamam.com/api/food-database/nutrients?app_id=edc61ca8&app_key=b9f17ae7284f840d6dd1ef3cbcdcde9e`,
         upcIngredients))
-      .then(res => console.log(res))
+      .then(res => isolateProductData(res))
       .catch(err => console.log(err))
   }
 
-
-  //   // Determines which day is currently selected by the user
-  //   function getDayFromDays(day, daysArray){
-  //     for (const d of daysArray){
-  //       if(d.name === day){
-  //         return d.spots
-  //       }
-  //     }
-  //   }
-  //   // Determine the index of the currently selected day in the array of days. 
-  //   function getDayIndex(day, daysArray){
-  //     for (const d of daysArray){
-  //       if(d.name === day){
-  //         return daysArray.indexOf(d)
-  //       }
-  //     }
-  //   }
-  // // subtracts from the number of spots remaining when a user books a new appointment. 
-  //   function subtractSpot(day) {
-  //     const spots = getDayFromDays(day, state.days) - 1
-  //     const index = getDayIndex(day, state.days)
-
-  //     setState((state) => ({
-
-  //       ...state,
-  //       ...state.days[index ]["spots"]=spots
-
-  //     })
-  //     )
-  // }
-  // // adds to the number of spots remaining when a user cancels an appointment. 
-  // function addSpot(day) {
-  //   const spots = getDayFromDays(day, state.days) + 1
-  //   const index = getDayIndex(day, state.days)
-
-  //   setState((state) => ({
-
-  //     ...state,
-  //     ...state.days[index ]["spots"]=spots
-
-  //   })
-  //   )
-  // }
-  // // submits a put request to the api to update the db with the details of a new appt
-  //   function bookInterview(id, interview) {
-  //     const appointment = {
-  //       ...state.appointments[id],
-  //       interview: { ...interview }
-  //     };
-
-  //   return  axios.put(`/api/appointments/${id}`, appointment)
-  //     .then(
-  //       (res) => {
-  //         setState((state) => ({
-
-  //           ...state,
-  //           appointments: {
-  //             ...state.appointments,
-  //             [id]: appointment
-  //           }
-  //         })
-  //         )
-  //       }
-
-  //     )
-  // }
-  // // submits a delete request to the api to update the db when and appointment is deleted
-  // function deleteInterview(id) {
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: null
-  //   };
-
-  // return  axios.delete(`/api/appointments/${id}`)
-  //   .then(
-  //     (res) => {
-  //       setState((state) => ({
-
-  //         ...state,
-  //         appointments: {
-  //           ...state.appointments,
-  //           [id]: appointment
-  //         }
-  //       })
-  //       )
-  //     }
-
-  //   )
-  // }
+  const isolateProductData = function (res) {
+    const healthLabels = res.data.healthLabels;
+    const dietLabels = res.data.dietLabels;
+    setProduct(product.healthTags = healthLabels)
+    setProduct(product.dietTags = dietLabels)
+    console.log("THIS IS THE RESPONSE", res)
+    console.log("THIS IS OUR PRODUCT", product)
+  }
 
   return {
     FetchApiData
