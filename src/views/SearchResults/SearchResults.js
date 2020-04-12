22070f172fd7d248e2604610e8a7f7f37862c0d9
Berk Ozer <galipberkozer@gmail.com>
2020-04-12 16:46:30 -0400

    Set up page structure for SearchResults

--- src/views/SearchResults/SearchResults.js
+++ src/views/SearchResults/SearchResults.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  },
+  content: {
+    marginTop: theme.spacing(5)
+  }
+}));
+
 
 const SearchResults = props => {
-  const { searchTerm } = props.location.state
+  const { searchTerm } = props.location.state;
+
+  const classes = useStyles();
+
   return (
-    <div>{searchTerm}</div>
+    <div className={classes.root}>
+      <Typography
+        align="center"
+        variant="h4"
+      >
+        Please choose the food you would like to see:
+      </Typography>
+      <div className={classes.content}>
+
+      </div>
+    </div>
   )
 }
 
