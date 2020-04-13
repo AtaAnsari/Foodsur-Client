a02281902c13f85e853d40bdc14bf2f12f8ef979
Berk Ozer <galipberkozer@gmail.com>
2020-04-13 14:16:10 -0400

    Add arrow to go back in SearchResults

--- src/views/SearchResults/SearchResults.js
+++ src/views/SearchResults/SearchResults.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { SearchResultsTable } from './components'
+
 import { getSearchResults } from 'helpers/getSearchResults'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(4)
+    padding: theme.spacing(4),
+    paddingTop: theme.spacing(2)
+  },
+  backArrow: {
+    paddingBottom: theme.spacing(2)
   }
 }));
 
 
 const SearchResults = props => {
   const [results, setResults] = useState('');
+  
+  const history = useHistory();
 
   const classes = useStyles();
 
@@ -23,8 +33,17 @@ const SearchResults = props => {
       .then(newResults => setResults(newResults));
   }, []);
 
+  const handleBack = () => {
+    history.goBack();
+  };
+
   return (
     <div className={classes.root}>
+      <div className={classes.backArrow}>
+        <IconButton onClick={handleBack}>
+          <ArrowBackIcon />
+        </IconButton>
+      </div>
       {results &&
       <SearchResultsTable searchResults={results} />}
     </div>
