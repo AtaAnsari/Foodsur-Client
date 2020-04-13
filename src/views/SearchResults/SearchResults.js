9d8141cc1b89b7c407c0c7280226f7c91c7a609c
Berk Ozer <galipberkozer@gmail.com>
2020-04-13 11:26:38 -0400

    Render info text in SearchResultsTable instead of SearchResults

--- src/views/SearchResults/SearchResults.js
+++ src/views/SearchResults/SearchResults.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { SearchResultsTable } from './components'
 import { getSearchResults } from 'helpers/getSearchResults'
@@ -7,9 +6,6 @@ import { getSearchResults } from 'helpers/getSearchResults'
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(4)
-  },
-  content: {
-    marginTop: theme.spacing(5)
   }
 }));
 
@@ -29,16 +25,8 @@ const SearchResults = props => {
 
   return (
     <div className={classes.root}>
-      <Typography
-        align="center"
-        variant="h4"
-      >
-        Please choose the food item you would like to see:
-      </Typography>
-      <div className={classes.content}>
-        {results &&
-        <SearchResultsTable searchResults={results} />}
-      </div>
+      {results &&
+      <SearchResultsTable searchResults={results} />}
     </div>
   )
 }
