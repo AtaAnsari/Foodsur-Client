e027647aa9aa77ddff54a4e3fdde4b4b934f688e
Berk Ozer <galipberkozer@gmail.com>
2020-04-13 11:27:35 -0400

    Render "please choose" text if we got SearchResults, "no results" text otherwise

--- src/views/SearchResults/components/SearchResultsTable/SearchResultsTable.js
+++ src/views/SearchResults/components/SearchResultsTable/SearchResultsTable.js
@@ -4,28 +4,50 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Typography,
   Paper
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  content: {
+    marginTop: theme.spacing(5)
+  }
+}));
 
 const SearchResultsTable = ({ searchResults }) => {
+  const classes = useStyles();
 
   return (
-    <Paper>
-      <Table>
-        <TableBody>
-          {searchResults.map((item, idx) => (
-            <TableRow
-              key={idx}
-              onClick={() => console.log(item.food.label)}
-            >
-              <TableCell>
-                {item.food.label}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </ Table>
-    </Paper>
+    <>
+      <Typography
+        align="center"
+        variant="h4"
+      >
+        {searchResults.length === 0 ?
+          'No results. Please try a different search term' :
+          'Please choose the food item you would like to see:'
+        }
+      </Typography>
+      <div className={classes.content}>
+        <Paper>
+          <Table>
+            <TableBody>
+              {searchResults.map((item, idx) => (
+                <TableRow
+                  key={idx}
+                  onClick={() => console.log(item.food.label)}
+                >
+                  <TableCell>
+                    {item.food.label}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </ Table>
+        </Paper>
+      </div>
+    </>
   )
 }
 
