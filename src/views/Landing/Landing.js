ed9b7aad4d942eb7d9dc4bb5e4bb59934053fbe2
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-14 13:57:51 -0400

    add translucent background to landing page tagline and fix position

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Button, Container, Typography } from '@material-ui/core';
+import { Button, Container, Typography, Box } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(4),
-    paddingTop: theme.spacing(20),
+    paddingTop: theme.spacing(25),
     backgroundImage: "url('/images/landing-bg.jpg')",
     height: "100%",
     backgroundRepeat: "no-repeat",
@@ -20,7 +21,14 @@ const useStyles = makeStyles(theme => ({
   },
   cardStyle: {
     padding: '1rem'
-  }
+  },
+  backgroundFilter: {
+    backgroundColor: fade(theme.palette.primary.main, 0.5)
+  },
+    tagLine: {
+      color: theme.palette.black
+    }
+  
 }));
 
 
@@ -39,24 +47,27 @@ const Landing = () => {
   const classes = useStyles()
   return (
     <Container className={classes.root}>
-      <Typography variant='h1' gutterBottom={true} align={'center'} color={'text'}>
-        Find the Right Food For You
-      </Typography>
-      <Button variant='contained'
-        color='primary'
-        fullWidth={true}
-        className={classes.buttonStyle}
-        onClick={handleLogin}>
-        Login
-      </Button>
-      <Button
-        variant='contained'
-        color='primary'
-        fullWidth={true}
-        className={classes.buttonStyle}
-        onClick={handleSignUp}>
-        Sign up
-      </Button>
+        <div className={classes.backgroundFilter}>
+          <Typography className={classes.tagLine} variant='h1' gutterBottom={true} align={'center'} color={'text'}>
+            Find the Right Food For You
+          </Typography>
+        </div>
+        <Button variant='contained'
+          color='primary'
+          fullWidth={true}
+          className={classes.buttonStyle}
+          onClick={handleLogin}>
+          Login
+        </Button>
+        <Button
+          variant='contained'
+          color='primary'
+          fullWidth={true}
+          className={classes.buttonStyle}
+          onClick={handleSignUp}>
+          Sign up
+        </Button>
+        
     </Container >
   )
 }
