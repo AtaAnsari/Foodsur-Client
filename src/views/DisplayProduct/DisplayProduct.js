48eb3bdfffa0d5abe6f000f6f4ff86bc3e39c08b
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-14 02:58:16 -0400

    add product expander which allows user click on a favourite button or view product details

--- src/views/DisplayProduct/DisplayProduct.js
+++ src/views/DisplayProduct/DisplayProduct.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import useUserRestrictions from 'hooks/useUserRestrictions'
-import { RestrictionCard } from './components'
-import { PassCard } from './components'
-import {Typography} from '@material-ui/core';
+import { RestrictionCard, PassCard, ProductExpander } from './components'
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  }
+
+}));
 
 const DisplayProduct = (props) => {
 
-  const {compareRestrictions} = useUserRestrictions();
-  
+const classes = useStyles();
 
-  const product = props.location.state.product
-  const {sharedRestricitions, divergentRestrictions} = compareRestrictions(product)
+const {compareRestrictions} = useUserRestrictions();
+
+const product = props.location.state.product
+
+const {sharedRestricitions, divergentRestrictions} = compareRestrictions(product)
+
 const formattedShared = sharedRestricitions.map(tag => tag.split('_').join(' '))
-const formattedDivergent = divergentRestrictions.map(tag => tag.split('_').join(' '))
-console.log('formatted', formattedShared);
-  const productName = props.location.state.product.productName
-  const shared = formattedShared.map(tag => <PassCard tag={tag} />)
-  const divergent = formattedDivergent.map(tag => <RestrictionCard tag={tag} />)
 
+const formattedDivergent = divergentRestrictions.map(tag => tag.split('_').join(' '))
+
+const productName = props.location.state.product.productName
+
+const shared = formattedShared.map(tag => <PassCard tag={tag} />)
+
+const divergent = formattedDivergent.map(tag => <RestrictionCard tag={tag} />)
 
   return (
     <div>
-      {/* <Typography variant="h3">{productName}</Typography> */}
-      <div>{shared}</div>
-      <div>{divergent}</div>
+      <div className={classes.root}>
+        <ProductExpander productName={productName}/>
+        <div>{shared}</div>
+        <div>{divergent}</div>
+      </div>
     </div>
-       
   )
 }
 
