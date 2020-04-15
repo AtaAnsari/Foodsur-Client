cfc86fb88f588bae82ff7ac61aec85ead4ae429f
Berk Ozer <galipberkozer@gmail.com>
2020-04-15 19:10:23 -0400

    Build handleCookies to set a "session" and "restrictions" cookie

--- src/helpers/handleCookies.js
+++ src/helpers/handleCookies.js
@@ -2,3 +2,9 @@ import { useCookies } from 'react-cookie';
 
 const [cookies, setCookie] = useCookies(['session']);
 
+const handleCookies = (userId, userRestrictions) => {
+  setCookie('session', userId, { path: '/' });
+  setCookie('restrictions', userRestrictions, { path: '/' });
+}
+
+export default handleCookies;
