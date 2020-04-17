2f59fef167c4992dee1377598e4b870eb2a9f943
Kevin <kevin.m.guertin@gmail.com>
2020-04-17 10:52:16 -0400

    connect popular-products route to backend

--- src/views/PopularProducts/PopularProducts.js
+++ src/views/PopularProducts/PopularProducts.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const PopularProducts = () => {
+
+  const getPopularProducts = () => {
+    axios.get('/api/user-data/popular-products', (req, res) => {
+      console.log('Sent...')
+    })
+  }
+
+  useEffect(() => {
+    getPopularProducts()
+  }, [])
+
   return (
     <h1>Hello</h1>
   )
