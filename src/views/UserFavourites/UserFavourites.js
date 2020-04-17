b43e76f86aa1a083836f6241c1a55f075b642ce5
Berk Ozer <galipberkozer@gmail.com>
2020-04-17 19:35:29 -0400

    useLoginValidation in UserFavourites and cancel promise of getUserFavourites on unmount

--- src/views/UserFavourites/UserFavourites.js
+++ src/views/UserFavourites/UserFavourites.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/styles';
 import { Container, TextField, Typography, Button } from '@material-ui/core';
 import { UserFavouritesCard } from './components'
 import axios from 'axios'
+import useLoginValidation from 'hooks/useLoginValidation';
 
 const useStyles = makeStyles(theme => {
   root: {
@@ -11,21 +12,27 @@ const useStyles = makeStyles(theme => {
 })
 
 const UserFavourites = () => {
-
   const classes = useStyles()
-
   const [favourites, setFavourites] = useState([])
 
+  useLoginValidation();
+  
   async function getUserFavourites() {
     const userData = await axios.get('/api/user-data/user-favourites')
     return userData
   }
 
+  // Sets favourites to user favourites (will stop setting of state if not logged in)
   useEffect(() => {
+    let loggedIn = true;
     getUserFavourites()
-      .then(product => setFavourites(product.data))
-  }
-    , [])
+      .then(product => {
+        if (loggedIn) {
+          setFavourites(product.data);
+        }
+      });
+    return () => loggedIn = false;
+  }, [])
 
   return (
     <Container className={classes.root}>
