09dba5e03cc1b1b4e5c170cae0b5c11e39fc6fbc
Berk Ozer <galipberkozer@gmail.com>
2020-04-18 18:07:46 -0400

    Use searchResults prop instead of fetching results in SearchResults.js

--- src/views/SearchResults/SearchResults.js
+++ src/views/SearchResults/SearchResults.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { SearchResultsTable } from './components'
-
-import { getSearchResults } from 'helpers/getSearchResults'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,33 +17,20 @@ const useStyles = makeStyles(theme => ({
 
 
 const SearchResults = props => {
-  const [results, setResults] = useState('');
-  
   const history = useHistory();
 
   const classes = useStyles();
 
-  const { searchTerm } = props.location.state;
-
-  // Fetch the search results and set as state
-  useEffect(() => {
-    getSearchResults(searchTerm)
-      .then(newResults => setResults(newResults));
-  }, []);
-
-  const handleBack = () => {
-    history.push("/home");
-  };
+  const { searchResults } = props.location.state;
 
   return (
     <div className={classes.root}>
       <div className={classes.backArrow}>
-        <IconButton onClick={handleBack}>
+        <IconButton onClick={history.goBack}>
           <ArrowBackIcon />
         </IconButton>
       </div>
-      {results &&
-      <SearchResultsTable searchResults={results} />}
+      <SearchResultsTable searchResults={searchResults} />
     </div>
   )
 }
