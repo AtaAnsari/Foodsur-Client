a0ca7ae5bad147f44064eb880ce0835f17f12c68
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-18 02:23:57 -0400

    make back button functional

--- src/views/DisplayProduct/components/SummaryCardPass/SummaryCardPass.js
+++ src/views/DisplayProduct/components/SummaryCardPass/SummaryCardPass.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles';
-import { Card, CardHeader, Divider, CardContent, Typography, Button, Box } from '@material-ui/core';
+import { Card, CardHeader, Divider, CardContent, Typography, Button, Box, IconButton } from '@material-ui/core';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { useHistory } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -35,13 +36,14 @@ const useStyles = makeStyles((theme) => ({
 const SummaryCardPass = (props) => {
 
 const [cookies] = useCookies(['session']);
+const history = useHistory();
 
-  const addFavourite = () => {
+const addFavourite = () => {
     const { product } = props
     const { dietTags, healthTags } = product
     const productTags = dietTags.concat(healthTags)
 
-    const productDetails = {
+const productDetails = {
       productName: props.productName,
       api_id: props.productId,
       productTags,
@@ -52,13 +54,18 @@ const [cookies] = useCookies(['session']);
 
     console.log("PRODUCT DETAILS", productDetails)
   }
+  const handleBack = function() {
+    history.push("/home");
+  };
   const classes = useStyles();
   return (
     <div>
       <Card>
         <div style={{display:"flex"}}>
         <div className={classes.backButton}>
-        <ArrowBackIcon/>
+        <IconButton onClick={handleBack}>
+        <ArrowBackIcon />
+        </IconButton>
         </div>
         <CardHeader
           title={props.productName}
