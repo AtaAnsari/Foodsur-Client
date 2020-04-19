30410ec2e5058e379c065712c8ed39b57b357e05
Kevin <kevin.m.guertin@gmail.com>
2020-04-19 18:18:55 -0400

    set up media view for desktop, some style

--- src/DesktopView.js
+++ src/DesktopView.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles';
+import { Container, Box, Typography, Grid } from '@material-ui/core';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundImage: "url('/images/landing-bg.jpg')",
+    height: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    flexDirection: "column",
+    display: "flex",
+    alignItems: "center",
+    justtifyContent: 'center'
+  },
+  backgroundFilter: {
+    backgroundColor: fade('#444444', 0.8)
+  }
+}))
 
 export default function DesktopView () {
+
+  const classes = useStyles()
+
   return (
-    <h1> DesktopView</h1>
+    <Box className={classes.root}>
+      <Grid>
+      <Box className={classes.backgroundFilter}>
+        <Box width='70%' >
+        <Typography 
+          className={classes.textStyle} 
+          variant='h2' gutterBottom={true} 
+          align={'center'}
+          color='white'>
+          Hello
+        </Typography>
+      </Box>
+      </Box>
+      </Grid>
+
+    </Box>
   )
 }
