b744ed63f061306a19cd56a531be967e508bcf23
Berk Ozer <galipberkozer@gmail.com>
2020-04-20 21:56:01 -0400

    Convert spacing units to responsive units in DesktopView.js

--- src/DesktopView.js
+++ src/DesktopView.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/styles';
 import { Box, Typography } from '@material-ui/core';
-import { fade } from '@material-ui/core/styles/colorManipulator';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles(() => ({
   root: {
     backgroundImage: 'url(\'/images/landing-bg.jpg\')',
     height: '100%',
@@ -12,7 +15,7 @@ const useStyles = makeStyles(theme => ({
     backgroundSize: 'cover',
     backgroundAttachment: 'fixed'
   },
-  textContainer: {
+  content: {
     height: '100%',
     display: 'flex',
     justifyContent: 'center',
@@ -22,14 +25,14 @@ const useStyles = makeStyles(theme => ({
   },
   text: {
     width: '60%',
-    marginBottom: '40px',
-    padding: '40px',
+    marginBottom: '4vh',
+    padding: '2.5vw',
     backgroundColor: 'rgba(255, 215, 0, .5)',
     borderRadius: '20px'
   },
   logo: {
-    width: '680px',
-    marginBottom: '110px'
+    width: '43vw',
+    marginBottom: '10vh'
   }
 }))
 
@@ -38,34 +41,36 @@ export default function DesktopView() {
   const classes = useStyles()
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.textContainer}>
-        <img
-          alt="Foodsur Logo"
-          className={classes.logo}
-          src="/images/logos/foodsur.png"
-        />
-        <Box className={classes.text}>
-          <Typography
-            align={'center'}
-            color="textPrimary"
-            gutterBottom
-            variant="h3"
-          >
-              Foodsur is a Progressive Web Application that is best viewed on mobile.
-          </Typography>
-        </Box>
-        <Box className={classes.text}>
-          <Typography
-            align={'center'}
-            color="textPrimary"
-            gutterBottom
-            variant="h4"
-          >
-              Please switch to responsive view (iPhone 6/7/8 etc.) or view the app on your mobile phone.
-          </Typography>
+    <ThemeProvider theme={theme}>
+      <Box className={classes.root}>
+        <Box className={classes.content}>
+          <img
+            alt="Foodsur Logo"
+            className={classes.logo}
+            src="/images/logos/foodsur.png"
+          />
+          <Box className={classes.text}>
+            <Typography
+              align={'center'}
+              color="textPrimary"
+              gutterBottom
+              variant="h3"
+            >
+                Foodsur is a Progressive Web Application that is best viewed on mobile.
+            </Typography>
+          </Box>
+          <Box className={classes.text}>
+            <Typography
+              align={'center'}
+              color="textPrimary"
+              gutterBottom
+              variant="h4"
+            >
+                Please switch to responsive view (iPhone 6/7/8 etc.) or view the app on your mobile phone.
+            </Typography>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   )
 }
